6603a111fa982680eb625d2c17d5b7da7851b640
LMatos31 <114191747+LMatos31@users.noreply.github.com>
2022-09-23 00:03:33 +0200

    Add project files.

---
+++ FindBirdsByDescription/Birds.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace FindBirdsByDescription
+{
+    public class Birds
+    {
+        public string Name { get; set; }
+        public string Colour { get; set; }
+        public string Desciprtion { get; set; }
+    }
+}
